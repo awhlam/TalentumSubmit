7d1479cbbbe771bc43f824d77c03aba48c146168
Lam <andrew.lam@fticonsulting.com>
2018-09-26 11:03:51 -0400

    Update Chrome binary path

--- TalentumSubmit.py
+++ TalentumSubmit.py
@@ -4,18 +4,21 @@
 
 # Import packages
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
 # Setup web driver
 chromedriver = "chromedriver.exe"
-browser = webdriver.Chrome(executable_path=chromedriver)
+options = Options()
+options.binary_location = 'C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe'
+browser = webdriver.Chrome(chromedriver, chrome_options=options)
 
 # Load Talentum website
 browser.get("https://apex.ftianalytics.com/ords/w_001000_0007/f?p=269:LOGIN::::::")
 
 # Submit username/password to login
-browser.find_element_by_id("P101_USERNAME").send_keys("YOURTALENTUMUSERNAME")
-browser.find_element_by_id("P101_PASSWORD").send_keys("YOURTALENTUMPASSWORD")
+browser.find_element_by_id("P101_USERNAME").send_keys("ALAM3")
+browser.find_element_by_id("P101_PASSWORD").send_keys("2%NazqZ8Vbffgh8f")
 browser.find_element_by_id("P101_LOGIN").click()
 
 # Edit and Submit Forecast
@@ -23,4 +26,4 @@ browser.find_element_by_link_text("Edit Time Forecast").click()
 browser.find_element_by_link_text("Submit Forecast").click()
 
 # Close web driver
-driver.quit()
+browser.quit()
