a091a6c9d016dc8d4df7bee25656db37191aaf43
Andrew Lam <32132177+awhlam@users.noreply.github.com>
2019-01-02 19:51:56 -0800

    Split into modular methods

--- TalentumSubmit.py
+++ TalentumSubmit.py
@@ -1,29 +1,25 @@
-# Talentum Submit
-# Version 1.0
-# 03/09/18
-
-# Import packages
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
-# Setup web driver
-chromedriver = "chromedriver.exe"
-options = Options()
-options.binary_location = 'C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe'
-browser = webdriver.Chrome(chromedriver, chrome_options=options)
+if __name__ == "__main__":
+    # Setup web driver
+    chromedriver = "chromedriver.exe"
+    options = Options()
+    options.binary_location = 'C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe'
+    browser = webdriver.Chrome(chromedriver, chrome_options=options)
 
-# Load Talentum website
-browser.get("https://apex.ftianalytics.com/ords/w_001000_0007/f?p=269:LOGIN::::::")
+    # Load Talentum website
+    browser.get("https://apex.ftianalytics.com/ords/w_001000_0007/f?p=269:LOGIN::::::")
 
-# Submit username/password to login
-browser.find_element_by_id("P101_USERNAME").send_keys("USERNAME")
-browser.find_element_by_id("P101_PASSWORD").send_keys("PASSWORD")
-browser.find_element_by_id("P101_LOGIN").click()
+    # Submit username/password to login
+    browser.find_element_by_id("P101_USERNAME").send_keys("USERNAME")
+    browser.find_element_by_id("P101_PASSWORD").send_keys("PASSWORD")
+    browser.find_element_by_id("P101_LOGIN").click()
 
-# Edit and Submit Forecast
-browser.find_element_by_link_text("Edit Time Forecast").click()
-browser.find_element_by_link_text("Submit Forecast").click()
+    # Edit and Submit Forecast
+    browser.find_element_by_link_text("Edit Time Forecast").click()
+    browser.find_element_by_link_text("Submit Forecast").click()
 
-# Close web driver
-browser.quit()
+    # Close web driver
+    browser.quit()
